69e3d9ba9ae6c232aba54b00331e9aabfc565301
matvey202020 <matvey.tokarev.1020@mail.ru>
2026-02-06 16:23:19 +0300

    обновление

--- main.js
+++ main.js
@@ -22,4 +22,25 @@ document.addEventListener('DOMContentLoaded', () => {       /* DOMContentLoaded 
     });
 
 })
-    
+
+/* Программирование логики фильтрации и сортировки */
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modelsSection = document.querySelector('.models_section');
+    /* Находим все элементы на странице, собираем их в список и делаем из этого списка полноценный массив */
+    const cards = Array.from(document.querySelectorAll('.computer_card'));
+    const gpuSelect = document.getElementById('gpuFilter');
+    const cpuSelect = document.getElementById('cpuFilter');
+    const applyBtn = document.getElementById('applyFilters');
+    const resetBtn = document.getElementById('resetFilters');
+
+    // Извлекаем данные из карточек и создаем массив обЪектов
+    // map - берет массив и создает новый массив в котором каждый элемент прошел через нашу функцию
+    let cardData = cards.map(card => {
+        const name = card.querySelector('h3').textContent.trim();   // textContetn - берет весь текстовый контект внутри тега h3
+        //.trim() - уберает все пробелы, переносы строк, табуляции с начала и конца
+        const priceStr = card.querySelector('table tr:nth-child(1) td:nth-child(2)').textContent.trim().replace('₽', '') // replace - заменяет один элемент на другой
+        const price = parseInt(priceStr,10) // parseInt - превращает строку в нормальное число в указанной системе исчесления
+    })
+
+})
